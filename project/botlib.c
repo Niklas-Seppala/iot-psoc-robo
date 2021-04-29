67571221b7f89763921166e2147b4c5a25e4768b
Niklas Seppälä <niklas.t.seppala@gmail.com>
2021-04-29 09:38:59 +0300

    project, sumo

---
+++ project/botlib.c
@@ -0,0 +1,132 @@
+#include "botlib.h"
+
+void io_wait_SW1() 
+{
+    while(FOREVER) 
+    {
+        if (SW1_Read() == BTN_PRESSED)
+        {
+            while (SW1_Read() != BTN_RELEASED);
+            break;
+        }
+        vTaskDelay(50);
+    }
+}
+
+int io_SW1_pressed(void)
+{
+    if (SW1_Read() == BTN_PRESSED)
+    {
+        while (SW1_Read() != BTN_RELEASED);
+        return 1;
+    }
+    
+    return 0;
+}
+
+void io_print_refl(struct sensors_ *s) 
+{
+    printf("%d%12d%12d%13d%15d%15d\n",
+        s->L3, s->L2, s->L1,
+        s->R1, s->R2, s->R3);     
+}
+
+void tank_turn(int direction, int speed, int duration)
+{
+    if (direction == RIGHT)
+    {
+        SetMotors(FORWARD, BACKWARD, speed, speed, duration);
+    }
+    else if (direction == LEFT)
+    {
+        SetMotors(BACKWARD, FORWARD, speed, speed, duration);
+    }
+}
+
+void smart_tank_turn(int dir, int speed, struct sensors_ * sensors)
+{
+    while (sensors->L1 || sensors->R1) 
+    {
+        tank_turn(dir, speed, 0);   
+        reflectance_digital(sensors);
+    }
+    while (!sensors->L1 || !sensors->R1) 
+    {
+        tank_turn(dir, speed, 0);   
+        reflectance_digital(sensors);
+    }
+    tank_turn(dir, speed, 24);
+}
+
+static int init_flags = -1;
+
+void *init(refl_conf_t *refl_confs, int flags) 
+{
+    printf("\n ******************\n         BOOT\n ******************\n");
+    
+    void *r_val = NULL;
+    if (refl_confs) 
+    {
+        r_val = calloc(1, sizeof(struct sensors_));
+        reflectance_start();
+        reflectance_set_threshold(
+            refl_confs->L3, refl_confs->L2,
+            refl_confs->L1, refl_confs->R1,
+            refl_confs->R2, refl_confs->R3);
+    }
+    
+    if (flags & INIT_ULTRA)
+    {
+        Ultra_Start();
+        vTaskDelay(30); // fix 0 read at the start
+    }
+    
+    if (flags & INIT_IR)
+    {
+        IR_Start();
+        IR_flush();
+    }
+    if (flags & INIT_MOTOR)
+    {
+        motor_start();
+        motor_forward(0, 0);
+    }
+    if (flags & INIT_ULTRA)
+    {
+        srand((unsigned)time(NULL));
+    }
+    
+    init_flags = flags;
+    return r_val;
+}
+
+void shutdown(void **h_allocated)
+{
+    if (init_flags > 0) 
+    {
+        if (*h_allocated) 
+        {
+            free(*h_allocated);
+            *h_allocated = NULL;
+        }    
+        
+        if (init_flags & INIT_MOTOR)
+        {
+            motor_forward(0,0);
+            motor_stop();
+        }
+    }
+    while(FOREVER);
+}
+
+int on_line(struct sensors_ *refl_sensors) 
+{
+    struct sensors_ *s = (struct sensors_*) refl_sensors;
+    return s->L3 && s->R3;
+}
+
+
+int rand_range(int min, int max)
+{
+    return (rand() % (max - min + 1)) + min;
+}
--- project/main.c
+++ project/main.c
@@ -1,1 +1,101 @@
-void zmain(void) {}
+#include "botlib.h"
+
+#define PROJECT_1 1
+#define PROJECT_2 0
+#define PROJECT_3 0
+
+#if PROJECT_1
+
+#define DISTANCE_TRESHOLD 2
+#define SPEED 50
+// These turn degrees are linked to constant speed
+// defined earlier.
+#define TURN_90_DEG 525
+#define TURN_45_DEG 262
+    
+void navigate(struct sensors_ *sensors);
+    
+void zmain(void) 
+{
+    refl_conf_t confs = { 9000, 9000, 11000, 11000, 9000, 9000 };
+    int init_flags = INIT_MOTOR | INIT_ULTRA | INIT_IR | INIT_RAND;
+    struct sensors_ *sensors = init(&confs, init_flags);
+    
+    // Drive to the start line.
+    io_wait_SW1();
+    reflectance_digital(sensors);
+    while (!on_line(sensors)) 
+   {
+        motor_forward(SPEED, 0);
+        reflectance_digital(sensors);
+    }
+    motor_forward(0, 0);
+    
+    // Ready state. Wait for IR signal.
+    print_mqtt("zumo02/ready", " %s", "zumo");
+    IR_wait();
+    
+    // Enter the ring.
+    uint32_t start_ticks = xTaskGetTickCount();
+    print_mqtt("zumo02/start", " %lu", start_ticks);
+    motor_forward(SPEED, 1500);
+    
+    // Execute sumo behaviour.
+    while (!io_SW1_pressed())
+    {
+        navigate(sensors);
+        motor_forward(SPEED, 0);
+    }
+    
+    // Send run summary and hibernate.
+    print_mqtt("zumo02/stop", " %lu", xTaskGetTickCount());
+    print_mqtt("zumo02/time", " %lu", xTaskGetTickCount() - start_ticks);
+    shutdown((void**)&sensors);
+}
+
+void navigate(struct sensors_ *sensors)
+{
+    if (Ultra_GetDistance() < DISTANCE_TRESHOLD)
+    {
+        print_mqtt("zumo02/obstacle", " %lu", xTaskGetTickCount());
+        motor_backward(SPEED, 500);
+        tank_turn(rand_range(RIGHT, LEFT), SPEED, TURN_90_DEG);
+    }
+    reflectance_digital(sensors);
+    if (sensors->L3 && !sensors->R3)
+    {
+        // Left side is clipping -> turn right.
+        motor_backward(SPEED, 200);
+        tank_turn(RIGHT, SPEED, TURN_45_DEG);
+    }
+    else if (!sensors->L3 && sensors->R3)
+    {
+        // Right side is clipping -> turn left.
+        motor_backward(SPEED, 200);
+        tank_turn(LEFT, SPEED, TURN_45_DEG);
+    }
+    else if (on_line(sensors))
+    {
+        // Direct collision incoming.
+        motor_backward(SPEED, 200);
+        tank_turn(rand_range(RIGHT, LEFT), SPEED, TURN_90_DEG);
+    }
+}
+
+#endif
+#if PROJECT_2
+
+void zmain(void) 
+{
+    
+}
+
+#endif    
+#if PROJECT_3
+
+void zmain(void) 
+{
+    
+}
+
+#endif
